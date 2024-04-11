cb5cc0ce75d48c12f127948c26e2c9daf64e94bb
tushar <tusharkumarshah14394@gmail.com>
2024-04-11 10:35:08 +0530

    first question done

---
+++ Striver's A2Z (leetcode)/189. Rotate Array.cpp
@@ -0,0 +1,18 @@
+// Link:
+// https://leetcode.com/problems/rotate-array/
+
+// Time complexity: O(2n)
+// Space complexity: O(1)
+
+#include <vector>
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k =k%n;
+        reverse(nums.begin(), nums.begin() + n - k);
+        reverse(nums.begin() + n - k, nums.end());
+        reverse(nums.begin(), nums.end());
+    }
+};
