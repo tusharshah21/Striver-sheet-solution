d65c4a92e4eb6c507f3ab49db777570b15b28f2e
tushar <tusharkumarshah14394@gmail.com>
2024-04-12 09:41:27 +0530

    today's start

---
+++ Striver's A2Z (leetcode)/1. Two Sum.cpp
@@ -0,0 +1,19 @@
+// Link: https://leetcode.com/problems/two-sum/
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numToIndex;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];            
+            if (numToIndex.find(complement) != numToIndex.end()) {
+                return {numToIndex[complement], i};
+            }            
+            numToIndex[nums[i]] = i;
+        }        
+        return {};
+    }
+};
