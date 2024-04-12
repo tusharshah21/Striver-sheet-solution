bdb24ab37b6e3e42d7cd43ceb9c5e197911ca850
tushar <tusharkumarshah14394@gmail.com>
2024-04-12 11:49:23 +0530

    added 3rd

---
+++ Striver's A2Z (leetcode)/169. Majority Element.cpp
@@ -0,0 +1,29 @@
+// Link: https://leetcode.com/problems/majority-element/
+
+// Time complexity: O(2n)
+// Space complexity: O(1)
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int ele;
+        for (int i = 0; i < nums.size(); i++) {
+            if (count == 0) {
+                count = 1;
+                ele = nums[i];
+            } else if (nums[i] == ele)
+                count++;
+            else
+                count--;
+        }
+        int count1 = 0;
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] == ele)
+                count1++;
+
+        if (count1 > (nums.size() / 2))
+            return ele;
+        return -1;
+    }
+};
