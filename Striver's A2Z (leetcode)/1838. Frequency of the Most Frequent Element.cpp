b4aea081835e0a4d782eedfe47da9a093dd2e134
tushar <tusharkumarshah14394@gmail.com>
2024-04-17 14:11:28 +0530

    learning hash

---
+++ Striver's A2Z (leetcode)/1838. Frequency of the Most Frequent Element.cpp
@@ -0,0 +1,21 @@
+// Link: https://leetcode.com/problems/frequency-of-the-most-frequent-element/description/
+
+// Time complexity: O(nlogn)
+// Space complexity: O(1
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int ans = 1;
+        long long s = 0;
+        for (int i = 1, j = 0; i < nums.size(); ++i) {
+            s += 1LL * (nums[i] - nums[i - 1]) * (i - j);
+            while (s > k) {
+                s -= nums[i] - nums[j++];
+            }
+            ans = max(ans, i - j + 1);
+        }
+        return ans;
+    }
+};
