da13260f69fb17dcb12e8ba868c4a9ffd52059d7
tushar <tusharkumarshah14394@gmail.com>
2024-04-18 20:07:28 +0530

    today's initiative

---
+++ Striver's A2Z (leetcode)/15. 3Sum.cpp
@@ -0,0 +1,37 @@
+// Link: https://leetcode.com/problems/3sum/
+
+// Time complexity: O(n^2)
+// Space complexity: O(n)
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+    if (nums.size() < 3)
+      return {};
+    vector<vector<int>> ans;
+    ranges::sort(nums);
+    for (int i = 0; i + 2 < nums.size(); ++i) {
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+      int l = i + 1;
+      int r = nums.size() - 1;
+      while (l < r) {
+        const int sum = nums[i] + nums[l] + nums[r];
+        if (sum == 0) {
+          ans.push_back({nums[i], nums[l++], nums[r--]});
+          while (l < r && nums[l] == nums[l - 1])
+            ++l;
+          while (l < r && nums[r] == nums[r + 1])
+            --r;
+        } else if (sum < 0) {
+          ++l;
+        } else {
+          --r;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
