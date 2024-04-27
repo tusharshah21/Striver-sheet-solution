3a85fd82a24dcc23fd690df050f51c64adfaa7c9
tushar <tusharkumarshah14394@gmail.com>
2024-04-27 09:33:43 +0530

    similar to previous one

---
+++ Striver's A2Z (leetcode)/240. Search a 2D Matrix II.cpp
@@ -0,0 +1,24 @@
+// Link: https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+// Time complexity: O(log(n*m))
+// Space complexity: O(1)
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int row = 0, col = m - 1;
+
+        // traverse the matrix from (0, m-1):
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] < target)
+                row++;
+            else
+                col--;
+        }
+        return false;
+    }
+};
