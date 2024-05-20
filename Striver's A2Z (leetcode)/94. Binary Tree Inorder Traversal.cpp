49721893d8f28d4cb8eb9ac9e2ca92f9d424ae01
tushar <tusharkumarshah14394@gmail.com>
2024-05-20 10:25:40 +0530

    inorder traveral added

--- Striver's A2Z (leetcode)/94. Binary Tree Inorder Traversal.cpp
+++ Striver's A2Z (leetcode)/94. Binary Tree Inorder Traversal.cpp
@@ -1,4 +1,4 @@
-// Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
+// Link: https://leetcode.com/problems/binary-tree-inorder-traversal/
 
 // Time complexity: O(n)
 // Space complexity: O(n)
@@ -15,20 +15,17 @@
  * };
  */
 class Solution {
+    void inorder(TreeNode* root,vector<int>& ans){
+        if(root==nullptr)
+        return;
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-    preorder(root, ans);
-    return ans;
-  }
-
- private:
-  void preorder(TreeNode* root, vector<int>& ans) {
-    if (root == nullptr)
-      return;
-
-    ans.push_back(root->val);
-    preorder(root->left, ans);
-    preorder(root->right, ans);
+        inorder(root,ans);
+        return ans;
     }
 };
