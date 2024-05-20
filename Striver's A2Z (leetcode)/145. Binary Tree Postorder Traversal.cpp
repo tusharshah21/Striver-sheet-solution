c362c71fbe069561a65b6514e484d598335d7645
tushar <tusharkumarshah14394@gmail.com>
2024-05-20 10:31:56 +0530

    postorder traversal

---
+++ Striver's A2Z (leetcode)/145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,31 @@
+// Link: https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void postorder(TreeNode* root, vector<int>& ans){
+        if(root==nullptr)
+            return;
+        postorder(root->left,ans);
+        postorder(root->right,ans);
+        ans.push_back(root->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector <int> ans;
+        postorder(root,ans);
+        return ans;
+    }
+};
