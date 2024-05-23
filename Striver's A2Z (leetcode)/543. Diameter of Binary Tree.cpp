7262f711a393d7d3627142a3dc6d53d270785545
tushar <tusharkumarshah14394@gmail.com>
2024-05-23 16:14:16 +0530

    another one

---
+++ Striver's A2Z (leetcode)/543. Diameter of Binary Tree.cpp
@@ -0,0 +1,31 @@
+// Link: https://leetcode.com/problems/diameter-of-binary-tree/
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int height(TreeNode* root,int & diameter){
+        if(root==nullptr) return 0;
+        int lh=height(root->left,diameter);
+        int rh=height(root->right,diameter);
+        diameter=max(diameter,lh+rh);
+        return 1+max(lh,rh);
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter=0;
+        height(root,diameter);
+        return diameter;
+    }
+};
