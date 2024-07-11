171cc77cfd5709332b54e39eb9dff3381df8368e
tushar <tusharkumarshah14394@gmail.com>
2024-07-11 21:45:06 +0530

    this one is from today

---
+++ Harkirat_cohort_DSA/1732. Find the Highest Altitude.cpp
@@ -0,0 +1,17 @@
+// Link: https://leetcode.com/problems/find-the-highest-altitude/
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n=gain.size();
+        vector<int> alt(n+1,0);
+        for(int i=1;i<=n;i++){
+            alt[i]=alt[i-1]+gain[i-1];
+        }
+        int max_ele=*max_element(alt.begin(),alt.end());
+        return max_ele;
+    }
+};
