86685f916042ad518e41c9b94061a3f2413c3fc3
tushar <tusharkumarshah14394@gmail.com>
2024-10-23 23:56:46 +0530

    tree concept easy

---
+++ Blind 75 aheet/100. Same Tree.cpp
@@ -0,0 +1,35 @@
+// Link: https://leetcode.com/problems/same-tree/
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q != NULL) {
+            return false;
+        }
+        if (p != NULL && q == NULL) {
+            return false;
+        }
+        if (p == NULL && q == NULL) {
+            return true;
+        }
+        if (!p || !q)
+            return p == q;
+        return (p->val == q->val) && isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
+    }
+};
