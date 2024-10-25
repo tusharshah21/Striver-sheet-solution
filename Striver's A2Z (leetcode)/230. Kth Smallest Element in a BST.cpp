d38f6f84da7bf2460db6acdb2e6059b3c627f411
tushar <tusharkumarshah14394@gmail.com>
2024-10-25 23:55:53 +0530

    bst recursion

---
+++ Striver's A2Z (leetcode)/230. Kth Smallest Element in a BST.cpp
@@ -0,0 +1,34 @@
+// Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+// Time complexity: O(nlogn)
+// Space complexity: O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int count = 0;
+    int ans;
+    void inorder(TreeNode* root, int k){
+        if(!root)return;
+        inorder(root->left,k);
+        if(++count==k){
+            ans = root->val;
+            return;
+        }
+        inorder(root->right,k);
+    }
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root,k);
+        return ans;
+    }
+};
