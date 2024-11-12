5befd16e7214a5ddd67aabacf82a0826fff98d85
tushar kumar shah <tusharkumarshah14394@gmail.com>
2024-11-12 23:30:17 +0530

    tree side view

--- Striver's A2Z (leetcode)/199. Binary Tree Right Side View.cpp
+++ Striver's A2Z (leetcode)/199. Binary Tree Right Side View.cpp
@@ -3,17 +3,6 @@
 // Time complexity: O(n)
 // Space complexity: O(n)
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
     void get(TreeNode* root,vector<int> &ans, int count){
         if(!root)   return;
