df73a26c0715927411373723908cb45cc7945617
tushar kumar shah <tusharkumarshah14394@gmail.com>
2024-12-04 23:47:13 +0530

    extra code removed

--- Striver's A2Z (leetcode)/230. Kth Smallest Element in a BST.cpp
+++ Striver's A2Z (leetcode)/230. Kth Smallest Element in a BST.cpp
@@ -3,17 +3,6 @@
 // Time complexity: O(nlogn)
 // Space complexity: O(1)
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
     int count = 0;
     int ans;
